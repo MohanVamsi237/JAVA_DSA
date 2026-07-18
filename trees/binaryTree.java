1440df05002688c6f500a9cc9bfa5cc2666adc10
MohanVamsi237 <mohanvamsi237@gmail.com>
2026-07-18 22:25:54 +0530

    added level order traversal

--- trees/binaryTree.java
+++ trees/binaryTree.java
@@ -1,3 +1,4 @@
+import java.util.*;
 public class binaryTree {
     static class Node {
         int data;
@@ -55,6 +56,34 @@ public class binaryTree {
         postorder(root.right);
         System.out.print(root.data+" ");
     }
+
+    static void levelorder(Node root){
+        Queue<Node> q=new LinkedList<>();
+        q.add(root);
+        q.add(null);
+
+        while(!q.isEmpty()){
+            Node currNode = q.remove();
+            if(currNode==null){
+                System.out.println();
+                if(q.isEmpty()){
+                    break;
+                }
+                else{
+                    q.add(null);
+                }
+            }
+            else{
+                System.out.print(currNode.data+" ");
+                if(currNode.left!=null){
+                    q.add(currNode.left);
+                }
+                if(currNode.right!=null){
+                    q.add(currNode.right);
+                }
+            }
+        }
+    }
     static void main(String[] args) {
         int nodes[] = {1, 2, 4, -1, -1, 5, -1, -1, 3, -1, 6, -1, -1};
 
@@ -65,6 +94,8 @@ public class binaryTree {
         inorder(root);
         System.out.println();
         postorder(root);
+        System.out.println();
+        levelorder(root);
         System.out.println();
     }
 }
