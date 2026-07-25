08f701ecdd9c165b62a5d42fa4272334139cc1f8
MohanVamsi237 <mohanvamsi237@gmail.com>
2026-07-25 23:11:52 +0530

    added aonther method for diameter

--- trees/binaryTree.java
+++ trees/binaryTree.java
@@ -115,7 +115,7 @@ public class binaryTree {
         return myHeight;
     }
 
-    public static int diameter(Node root){
+    static int diameter(Node root){
         if(root==null){
             return 0;
         }
@@ -126,6 +126,33 @@ public class binaryTree {
         return Math.max(diam3,Math.max(diam1,diam2));
     }
 
+    static class TreeInfo{
+        int ht;
+        int diam;
+
+        TreeInfo(int ht,int diam){
+            this.ht=ht;
+            this.diam=diam;
+        }
+    }
+
+    static TreeInfo diameter2(Node root){
+        if(root==null){
+            return new TreeInfo(0,0);
+        }
+        TreeInfo left=diameter2(root.left);
+        TreeInfo right=diameter2(root.right);
+
+        int myHeight=Math.max(left.ht,right.ht)+1;
+
+        int diam1=left.diam;
+        int diam2=right.diam;
+        int diam3=left.ht+right.ht+1;
+
+        int mydiam=Math.max(Math.max(diam1, diam2), diam3);
+        TreeInfo myInfo=new TreeInfo(myHeight, mydiam);
+        return myInfo;
+    }
     
     static void main(String[] args) {
         int nodes[] = {1, 2, 4, -1, -1, 5, -1, -1, 3, -1, 6, -1, -1};
@@ -144,5 +171,7 @@ public class binaryTree {
         System.out.println("Sum of nodes: "+sumOfNodes(root));
         System.out.println("Height of tree: "+height(root));
         System.out.println("Diameter of tree: "+diameter(root));
+        System.out.println("Diameter of tree: "+diameter2(root).diam);
+
     }
 }
