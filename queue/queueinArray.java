d1ed3288361b01f03a4a679780a20c7946cce3ee
MohanVamsi237 <mohanvamsi237@gmail.com>
2026-07-26 22:16:38 +0530

    added a new file and made some changes in binaryTree file

---
+++ queue/queueinArray.java
@@ -0,0 +1,7 @@
+package queue;
+
+public class QueueInArray{
+    public static void main(String[] args){
+        
+    }
+}
--- trees/binaryTree.java
+++ trees/binaryTree.java
@@ -153,10 +153,11 @@ public class binaryTree {
         TreeInfo myInfo=new TreeInfo(myHeight, mydiam);
         return myInfo;
     }
+
     
+
     static void main(String[] args) {
         int nodes[] = {1, 2, 4, -1, -1, 5, -1, -1, 3, -1, 6, -1, -1};
-
         BinaryTree.idx = -1;
         Node root = BinaryTree.buildTree(nodes);
         preorder(root);
